10384c0ab66d3087865a82e899b9c257d207bde4
ricardomart26 <ricardomartins2626t@gmail.com>
2021-12-18 15:13:44 +0000

    Finished ex00, and started ex01, trying to figure out how to convert int to float

--- Cpp Module 02/ex00/Fixed.cpp
+++ Cpp Module 02/ex00/Fixed.cpp
@@ -1,6 +1,38 @@
 #include "Fixed.hpp"
 
-int	Fixed::getRawBits() const
+Fixed::Fixed()
 {
-	std::cout << "getRawBits called!" << std::endl;
+	std::cout << "Constructor called!" << std::endl;
+	setRawBits(0);
 }
+
+Fixed::~Fixed()
+{
+	std::cout << "Deconstructor called!" << std::endl;
+}
+
+Fixed::Fixed(const Fixed & copy)
+{
+	std::cout << "Copy constructor called!" << std::endl;
+	*this = copy;
+}
+
+Fixed & Fixed::operator = (const Fixed & obj)
+{
+	std::cout << "operator constructor called" << std::endl;
+	this->_fixedPoint = obj._fixedPoint;
+	return (*this);
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "Get raw bits called!" << std::endl;
+	return (this->_fixedPoint);
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	std::cout << "Set Raw bits called!" << std::endl;
+	this->_fixedPoint = raw;
+}
+
--- Cpp Module 02/ex00/Fixed.hpp
+++ Cpp Module 02/ex00/Fixed.hpp
@@ -4,10 +4,10 @@ class Fixed
 {
 	public:
 		
-		Fixed() {std::cout << "Default contructor called! " << std::endl;}
-		Fixed(const Fixed&) {std::cout << "Copy constructor called! " << std::endl;}
-		~Fixed() {std::cout << "Destructed Object! " << std::endl;}
-		Fixed & operator = (const Fixed &a) {std::cout << "Assignemt operator called !" << std::endl;}
+		Fixed();
+		Fixed(const Fixed& copy);
+		~Fixed();
+		Fixed & operator = (const Fixed &a);
 
 		int		getRawBits(void) const; // Retorna o valor raw do fixed point
 		void	setRawBits(const int raw); // Define o raw value do fixed point
