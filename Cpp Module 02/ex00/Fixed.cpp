18fc54626815ba16ab8012445f252c0b5da3e3cc
ricardomart26 <ricardomartins2626t@gmail.com>
2021-12-18 19:00:29 +0000

    to pull

--- Cpp Module 02/ex00/Fixed.cpp
+++ Cpp Module 02/ex00/Fixed.cpp
@@ -1,6 +1,23 @@
 #include "Fixed.hpp"
 
+// int	convert_to_binary()
+// {
+// 	int	bits;
+// 	int temp = _fixedPoint;
+// 	int	size = _staticPoint;
+
+// 	while (size-- > 0)
+// 	{
+// 		bits = temp % 2;
+// 		temp /= 2;
+// 		bits *= 10;
+// 	}
+// 	std::cout << "ret: " << temp << std::endl;
+// 	return (bits);
+// }
+
 int	Fixed::getRawBits() const
 {
 	std::cout << "getRawBits called!" << std::endl;
+	return _fixedPoint;
 }
--- Cpp Module 02/ex00/Fixed.hpp
+++ Cpp Module 02/ex00/Fixed.hpp
@@ -4,17 +4,22 @@ class Fixed
 {
 	public:
 		
-		Fixed() {std::cout << "Default contructor called! " << std::endl;}
+		Fixed() 
+			: _fixedPoint(0) 
+		{
+			std::cout << "Default contructor called! " << std::endl;
+		}
 		Fixed(const Fixed&) {std::cout << "Copy constructor called! " << std::endl;}
 		~Fixed() {std::cout << "Destructed Object! " << std::endl;}
 		Fixed & operator = (const Fixed &a) {std::cout << "Assignemt operator called !" << std::endl;}
 
+		int		convert_to_binary();
 		int		getRawBits(void) const; // Retorna o valor raw do fixed point
 		void	setRawBits(const int raw); // Define o raw value do fixed point
 
 	private:
 
 	int					_fixedPoint;
-	const static int	_staticPoint;
+	const static int	_staticPoint = 8;
 
 };
